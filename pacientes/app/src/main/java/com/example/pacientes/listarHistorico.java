d1e176e868128dc23eb3912ff2c9f7f148e195c9
Gabriel ribeiro <gabrelvital49@gmail.com>
2020-05-22 15:06:35 -0300

    Efetuando o inicio da listagem de Historico

---
+++ pacientes/app/src/main/java/com/example/pacientes/listarHistorico.java
@@ -0,0 +1,14 @@
+package com.example.pacientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listarHistorico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_historico);
+    }
+}
